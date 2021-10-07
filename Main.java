48630207308dfb8206b752d7fd09840c16eaa075
HSTDNT18 <92122088+Joseflores19@users.noreply.github.com>
2021-10-07 14:29:04 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hola Mi Nombre Es Espacialmente Espacial");
+  }
+}
+/*This is a comment*/
+//This is another way to comment
+/*Java Variables types
+You have 5 types of variablesin Java
+Boolean: True/False,only 2 state - boolean
+Integers: Int , always WHOLE numbers
+Strings: Text, ex "Hello", ALWAYS between quotation
+Floats: Decimals, ex 19.99 or -19.99
+Char: single characters, "A", "B", Write a code hat give you how many characters are in "pneumonoultramicroscopicsilicovolcanoconiosis"
+
+
+In math, x = 4 means 4 = x
+In programming x = 4 (4 is assigned to x)
+
+x = "patty"
+b = "tomato"
+c = "Cheese"
+d = "bread"
+e = "Lettuce"
+f = "Ketchup"
+g = "mayo"
+
+So the tamplate is: type variable = value;
+*/ 
